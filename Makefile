b64f871d53334107e23ab6e2d0ba2758f5d3ba11
Zifan Shi <vivianszf9@gmail.com>
2018-05-06 15:16:14 +0800

    add test cases

--- Makefile
+++ Makefile
@@ -15,5 +15,32 @@ lab1:
 	flex lexical.l
 	gcc main.c syntax.tab.c treeop.c -lfl -ly -o parser
 
-test:
-	./parser test.cmm
+testc1:
+	./parser test/compulsory/1.cmm
+
+testc2:
+	./parser test/compulsory/2.cmm
+
+testc3:
+	./parser test/compulsory/3.cmm
+
+testc4:
+	./parser test/compulsory/4.cmm
+
+testo1:
+	./parser test/others/1.cmm
+
+testo2:
+	./parser test/others/2.cmm
+
+testo3:
+	./parser test/others/3.cmm
+
+testo4:
+	./parser test/others/4.cmm
+
+testo5:
+	./parser test/others/5.cmm
+
+testo6:
+	./parser test/others/6.cmm
--- main.c
+++ main.c
@@ -6,18 +6,20 @@ extern int errornot;
 
 int main(int argc, char** argv){
     if(argc<=1) return 1;
-    FILE *f=fopen(argv[1], "r");
-    if(!f){
-        perror(argv[1]);
-        return 1;
-    }
-    printf("begin\n");
-    yyrestart(f);
-    errornot=0;
-    yyparse();
-    if(errornot==0){
-        preorderprint(root,0);
-    }
-    printf("finish!!!\n");
+	int i;
+	for(i=1;i<argc;i++){
+    	FILE *f=fopen(argv[1], "r");
+    	if(!f){
+        	perror(argv[1]);
+        	return 1;
+    	}
+    	yyrestart(f);
+    	errornot=0;
+   		yyparse();
+    	if(errornot==0){
+        	preorderprint(root,0);
+    	}
+		fclose(f);
+	}
     return 0;
 }
