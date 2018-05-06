41aa36cde8817d979d48af8cbcdac5e2b4898c87
Zifan Shi <vivianszf9@gmail.com>
2018-05-06 18:32:52 +0800

    fix tab problem

--- main.c
+++ main.c
@@ -8,27 +8,27 @@ extern int errorlineno;
 extern int yylineno;
 
 int main(int argc, char** argv){
-    if(argc<=1) return 1;
+	if(argc<=1) return 1;
 	int i;
 	for(i=1;i<argc;i++){
-    	FILE *f=fopen(argv[i], "r");
-    	if(!f){
-        	perror(argv[1]);
-        	return 1;
-    	}
-    	yyrestart(f);
+		FILE *f=fopen(argv[i], "r");
+		if(!f){
+			perror(argv[1]);
+			return 1;
+		}
+		yyrestart(f);
 		printf("In file %s:\n",argv[i]);
 		//initialization
-    	errornot=0;
+		errornot=0;
 		mark=-1;
 		errorlineno=-1;
 		yylineno=1;
 
-   		yyparse();
-    	if(errornot==0){
-        	preorderprint(root,0);
-    	}
+		yyparse();
+		if(errornot==0){
+			preorderprint(root,0);
+		}
 		fclose(f);
 	}
-    return 0;
+	return 0;
 }
